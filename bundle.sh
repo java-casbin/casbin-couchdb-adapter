4e85035b9f6e5ef2980b88f6b775f033cdf15318
Zike Yang <zike@apache.org>
2024-02-02 20:49:31 +0800

    Add License Checker (#2)
    * build: add license checker
    
    * build: add license checker
    
    * Fix license

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,19 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -xe
 
-BUNDLE_DIR=build/bundle/io/github/java-casbin/casbin-couchdb-adapter/1.0.0-SNAPSHOT
+VERSION=$(./gradlew -q printVersion)
+BUNDLE_DIR="build/bundle/io/github/java-casbin/casbin-couchdb-adapter/$VERSION"
 
 rm -rf build/bundle
 rm -rf build/libs
@@ -16,5 +29,6 @@ for file in $BUNDLE_DIR/*; do
 done
 
 pushd build/bundle
+rm -f ../bundle.zip
 zip -r ../bundle.zip *
 popd
